e7fb4e10285dc037d24cee9065e528d009ea4858
Kelvin Li <kelvin.li@emc.com>
2015-09-11 11:06:21 -0400

    01 project02 dev

--- gs-rest-service-complete/src/main/java/hello/GreetingController.java
+++ gs-rest-service-complete/src/main/java/hello/GreetingController.java
@@ -13,6 +13,7 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+		Syste,.out.println("yaoyaoqiekenao");
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
